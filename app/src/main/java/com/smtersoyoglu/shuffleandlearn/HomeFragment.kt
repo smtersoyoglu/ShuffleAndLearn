04b1021b90bc187913ed1e1fdfe9d3ada55f46da
smter <smtersoyoglu231@gmail.com>
2024-09-09 20:21:41 +0300

    Added SwipeRefreshLayout to Home page

--- app/src/main/java/com/smtersoyoglu/shuffleandlearn/HomeFragment.kt
+++ app/src/main/java/com/smtersoyoglu/shuffleandlearn/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.smtersoyoglu.shuffleandlearn
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.smtersoyoglu.shuffleandlearn.databinding.FragmentHomeBinding
 
@@ -15,6 +19,9 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var wordListAdapter: WordListAdapter
+    private lateinit var viewModel: WordViewModel
+    private lateinit var sharedPreferences: SharedPreferences
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,28 +34,30 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val wordList = getWordList()
-        wordListAdapter = WordListAdapter(wordList)
+        sharedPreferences = requireActivity().getSharedPreferences("learned_words", Context.MODE_PRIVATE)
+        viewModel = ViewModelProvider(this).get(WordViewModel::class.java)
+
+        wordListAdapter = WordListAdapter(arrayListOf()) { word ->
+            // Handle word click event
+            val action = HomeFragmentDirections.actionHomeFragmentToDetailFragment(word)
+            findNavController().navigate(action)
+        }
 
         binding.wordRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
         binding.wordRecyclerView.adapter = wordListAdapter
 
+        // Swipe to Refresh Setup
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            viewModel.shuffleWords()
+            binding.swipeRefreshLayout.isRefreshing = false
 
+        }
+
+        viewModel.wordList.observe(viewLifecycleOwner) { wordList ->
+            wordListAdapter.updateWordList(wordList)
+        }
     }
 
-    private fun getWordList(): ArrayList<Word> {
-        return arrayListOf(
-            Word(1, "Ördek", "Duck", R.drawable.duck_card),
-            Word(2, "Köpek", "Dog", R.drawable.dog_card),
-            Word(3, "Kedi", "Cat", R.drawable.cat_card),
-            Word(4, "Elma", "Apple", R.drawable.apple_card),
-            Word(5, "Ağaç", "Tree", R.drawable.tree_card),
-            Word(6, "Şişe", "Bottle", R.drawable.bottle_card),
-            Word(7, "Saat", "Watch", R.drawable.watch_card),
-            Word(8, "Sandalye", "Chair", R.drawable.chair_card),
-            Word(9, "Diş fırçası", "Toothbrush", R.drawable.toothbrush_card),
-        )
-    }
 
     override fun onDestroyView() {
         super.onDestroyView()
