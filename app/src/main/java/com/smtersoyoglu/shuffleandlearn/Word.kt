d8bee6ef16d8c8c9736a65c557408e49773906ab
smter <smtersoyoglu231@gmail.com>
2024-09-09 18:24:22 +0300

    Added @Parcelize to data class for parcelable functionality

--- app/src/main/java/com/smtersoyoglu/shuffleandlearn/Word.kt
+++ app/src/main/java/com/smtersoyoglu/shuffleandlearn/Word.kt
@@ -1,8 +1,12 @@
 package com.smtersoyoglu.shuffleandlearn
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Word(
     val id: Int,
     val translation: String,
     val english: String,
-    val imageResId: Int
-)
+    val imageResId: Int,
+    var isLearned: Boolean = false
+) : Parcelable
