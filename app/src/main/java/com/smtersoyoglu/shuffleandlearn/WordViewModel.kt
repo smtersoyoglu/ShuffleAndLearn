719400185dd8b9778e23bc1de471f3df16158b2e
smter <smtersoyoglu231@gmail.com>
2024-09-12 19:33:17 +0300

    Updated WordViewModel to integrate with WordRepository and enhance data handling - Integrated `WordRepository` with `WordDataSource` for data retrieval. - Replaced hardcoded word list retrieval with JSON data fetching in `fetchWordsFromJson`. - Enhanced data management by using `SharedPreferences` to store and retrieve learned words. - Added methods to handle learned words:   - `markWordAsLearned` and `markWordAsUnlearned` to update learned word status and manage lists accordingly.   - `saveLearnedWordsToPrefs` and `getLearnedWordsFromPrefs` to handle JSON serialization/deserialization with `SharedPreferences`. - Improved `shuffleWords` method for better list shuffling. - Added methods to manage word list updates when words are learned or unlearned.

--- app/src/main/java/com/smtersoyoglu/shuffleandlearn/WordViewModel.kt
+++ app/src/main/java/com/smtersoyoglu/shuffleandlearn/WordViewModel.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import org.json.JSONArray
+import org.json.JSONObject
 
 class WordViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository = WordRepository()
-    private val sharedPreferences = getApplication<Application>().getSharedPreferences("learned_words", Context.MODE_PRIVATE)
+    private val repository = WordRepository(WordDataSource(application))
 
     private val _wordList = MutableLiveData<ArrayList<Word>>()
     val wordList: LiveData<ArrayList<Word>> get() = _wordList
@@ -16,54 +19,114 @@ class WordViewModel(application: Application) : AndroidViewModel(application) {
     private val _learnedWordList = MutableLiveData<ArrayList<Word>>()
     val learnedWordList: LiveData<ArrayList<Word>> get() = _learnedWordList
 
+    private val sharedPreferences = getApplication<Application>().getSharedPreferences("learned_words", Context.MODE_PRIVATE)
+
     init {
-        fetchWords()
+        fetchWordsFromJson()
         fetchLearnedWords()
     }
 
-    fun fetchWords() {
+    fun fetchWordsFromJson() {
         val allWords = repository.getWordList()
-        val learnedWordsIds = getLearnedWordsIds()
-        _wordList.value = allWords.filter { !learnedWordsIds.contains(it.id) } as ArrayList<Word>
+        val learnedWords = getLearnedWordsFromPrefs()
+
+        val filteredWords = allWords.filter { word ->
+            learnedWords.none { it.id == word.id }
+        }
+
+        _wordList.postValue(ArrayList(filteredWords))
+    }
+
+    fun shuffleWords() {
+        _wordList.value?.let { wordList ->
+            wordList.shuffle()
+            _wordList.postValue(ArrayList(wordList))
+        }
     }
 
     fun fetchLearnedWords() {
-        val learnedWordsIds = getLearnedWordsIds()
-        val allWords = repository.getWordList()
-        _learnedWordList.value = allWords.filter { learnedWordsIds.contains(it.id) } as ArrayList<Word>
-    }
-
-    fun isWordLearned(wordId: Int): Boolean {
-        return sharedPreferences.contains("word_${wordId}_learned")
+        viewModelScope.launch {
+            val learnedWords = getLearnedWordsFromPrefs()
+            _learnedWordList.postValue(learnedWords)
+        }
     }
 
     fun markWordAsLearned(word: Word) {
-        val editor = sharedPreferences.edit()
-        editor.putBoolean("word_${word.id}_learned", true)
-        editor.apply()
+        val learnedWords = getLearnedWordsFromPrefs()
 
-        fetchWords()  // Refresh word list
-        fetchLearnedWords()  // Refresh learned words list
+        if (learnedWords.none { it.id == word.id }) {
+            val updatedList = ArrayList(learnedWords).apply {
+                add(word)
+            }
+            saveLearnedWordsToPrefs(updatedList)
+            _learnedWordList.postValue(updatedList)
+            removeWordFromWordList(word)
+        }
+
     }
 
     fun markWordAsUnlearned(word: Word) {
-        val editor = sharedPreferences.edit()
-        editor.remove("word_${word.id}_learned")
-        editor.apply()
 
-        fetchWords()  // Refresh word list
-        fetchLearnedWords()  // Refresh learned words list
+        val learnedWords = getLearnedWordsFromPrefs().apply {
+            remove(word)
+        }
+        saveLearnedWordsToPrefs(learnedWords)
+        _learnedWordList.postValue(learnedWords)
+        addWordToWordList(word)
     }
 
-    private fun getLearnedWordsIds(): Set<Int> {
-        return sharedPreferences.all.keys
-            .filter { it.startsWith("word_") }
-            .map { it.removePrefix("word_").removeSuffix("_learned").toInt() }
-            .toSet()
+    fun isWordLearned(wordId: Int): Boolean {
+        val learnedWords = getLearnedWordsFromPrefs()
+        return learnedWords.any { it.id == wordId }
     }
-    fun shuffleWords() {
-        _wordList.value = _wordList.value?.shuffled() as ArrayList<Word>
+
+    private fun getLearnedWordsFromPrefs(): ArrayList<Word> {
+        val learnedWordsJson = sharedPreferences.getString("learned_words", "[]")
+        val learnedWordsList = ArrayList<Word>()
+        val jsonArray = JSONArray(learnedWordsJson)
+
+        for (i in 0 until jsonArray.length()) {
+            val jsonObject = jsonArray.getJSONObject(i)
+            val id = jsonObject.getInt("id")
+            val english = jsonObject.getString("english")
+            val translation = jsonObject.getString("translation")
+            val imageUrl = jsonObject.getString("image_url")
+            val sentence = jsonObject.optString("sentence", "")
+
+            learnedWordsList.add(Word(id, translation, english, imageUrl, sentence = sentence))
+        }
+
+        return learnedWordsList
+    }
+
+    private fun saveLearnedWordsToPrefs(learnedWords: ArrayList<Word>) {
+        val jsonArray = JSONArray()
+
+        for (word in learnedWords) {
+            val jsonObject = JSONObject()
+            jsonObject.put("id", word.id)
+            jsonObject.put("english", word.english)
+            jsonObject.put("translation", word.translation)
+            jsonObject.put("image_url", word.imageUrl)
+            jsonObject.put("sentence", word.sentence)
+            jsonArray.put(jsonObject)
+        }
+
+        sharedPreferences.edit().putString("learned_words", jsonArray.toString()).apply()
+    }
+
+    private fun removeWordFromWordList(word: Word) {
+        _wordList.value?.let { wordList ->
+            wordList.remove(word)
+            _wordList.postValue(ArrayList(wordList))
+        }
+    }
+
+    private fun addWordToWordList(word: Word) {
+        _wordList.value?.let { wordList ->
+            wordList.add(word)
+            _wordList.postValue(ArrayList(wordList))
+        }
     }
 }
-
 
